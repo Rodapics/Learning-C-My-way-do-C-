b35b64d39abf730a4aa821666d8d35f050907741
Rodapics <rodallega.carlos@correounivalle.edu.co>
2025-02-11 23:06:38 -0500

    Coursera courses

---
+++ Aprendiendo/Cursos/Coursera/Course 3/Code_21 - copia (14) - copia - copia - copia - copia - copia.cpp
@@ -0,0 +1,27 @@
+
+
+//Activity: You are responsible for a rail convoy of goods consisting of several boxcars. You start the train and after a few minutes you realize that some boxcars are overloaded and weigh too heavily on the rails while others are dangerously light. So you decide to stop the train and spread the weight more evenly so that all the boxcars have exactly the same weight (without changing the total weight). For that you write a program which helps you in the distribution of the weight.
+
+//Your program should first read the number of cars to be weighed (integer) followed by the weights of the cars (doubles). Then your program should calculate and display how much weight to add or subtract from each car such that every car has the same weight. The total weight of all of the cars should not change. These additions and subtractions of weights should be displayed with one decimal place.
+
+//You may assume that there are no more than 50 boxcars. 
+
+//Using an IF statement inside a FOR loop: Find the largest array element.
+
+#include <stdio.h>
+int main(void) {
+    //! showArray(ages, cursors=[i])
+    int ages[10];
+    int i;
+    int ageMax = 0;
+    for (i=0; i<10; i++) {
+        scanf("%d", &ages[i]);
+        if (ages[i] > ageMax) {
+            ageMax = ages[i];
+        }
+    }
+    printf("The maximum age is %d.\n", ageMax);
+    return 0;
+}
+
+
---
+++ Aprendiendo/Libros/GuideToScientificComputingInC/Chapther_1_!.cpp
@@ -0,0 +1,13 @@
+// First Chapter: Gettin Stated Guide to scientific computing.
+// First Example:How to write the "Hello word".
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	cout<<"Hello World\n";
+	
+	return 0;
+}
